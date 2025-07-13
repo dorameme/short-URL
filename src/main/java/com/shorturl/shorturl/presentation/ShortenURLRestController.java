b57177bf87a75f65f4454366d5be43773f4b9b48
dorameme <doradorav@naver.com>
2025-07-13 14:27:12 +0900

    UI완성

--- src/main/java/com/shorturl/shorturl/presentation/ShortenURLRestController.java
+++ src/main/java/com/shorturl/shorturl/presentation/ShortenURLRestController.java
@@ -1,52 +1,53 @@
-package com.shorturl.shorturl.presentation;
+    package com.shorturl.shorturl.presentation;
 
 
-import com.shorturl.shorturl.application.SimpleShortenService;
-import jakarta.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+    import com.shorturl.shorturl.application.SimpleShortenService;
+    import jakarta.validation.Valid;
+    import java.net.URI;
+    import java.net.URISyntaxException;
+    import org.springframework.http.HttpHeaders;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.RequestBody;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RequestMethod;
+    import org.springframework.web.bind.annotation.ResponseBody;
+    import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ShortenURLRestController {
-    private SimpleShortenService simpleShortenService;
+    @RestController
+    public class ShortenURLRestController {
+        private SimpleShortenService simpleShortenService;
 
-    public ShortenURLRestController(SimpleShortenService simpleShortenService) {
-        this.simpleShortenService = simpleShortenService;
+        public ShortenURLRestController(SimpleShortenService simpleShortenService) {
+            this.simpleShortenService = simpleShortenService;
+        }
+
+        @RequestMapping(value = "/r/{shortenUrlKey}", method = RequestMethod.GET)
+        public ResponseEntity<?> redirectShortUrl( @PathVariable String shortenUrlKey) throws URISyntaxException
+        {
+            String originalUrl = simpleShortenService.getOriginalUrlByShortenUrlKey(shortenUrlKey);
+            URI redirectUri = new URI(originalUrl);
+            HttpHeaders httpHeaders =new HttpHeaders();
+            httpHeaders.setLocation(redirectUri);
+
+            return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
+
+        }
+        @RequestMapping(value = "/shortenUrl",method = RequestMethod.POST)
+        public ResponseEntity<ShortenUrlCreateResponseDto>  createShortenUrl(@Valid @RequestBody ShortenUrlCreateRequestDto shortenUrlCreateRequestDto){
+            ShortenUrlCreateResponseDto shortenUrlCreateResponseDto=
+                    simpleShortenService.generateShortUrl(shortenUrlCreateRequestDto);
+            return ResponseEntity.ok().body(shortenUrlCreateResponseDto);
+        }
+
+
+        @RequestMapping(value = "/shortenUrl/{shortenUrlKey}", method = RequestMethod.GET)
+        public ResponseEntity<?>  getShortenUrlInformation(        @PathVariable String shortenUrlKey){
+            ShortenUrlInformationDto shortenUrlInformationDto=
+                simpleShortenService.getShortenUrlInformationByShortenUrlKey(shortenUrlKey);
+            return ResponseEntity.ok(shortenUrlInformationDto);
+        }
+
     }
-
-    @RequestMapping(value = "/{shortenUrlKey}", method = RequestMethod.GET)
-    public ResponseEntity<?> redirectShortUrl( @PathVariable String shortenUrlKey) throws URISyntaxException
-    {
-        String originalUrl = simpleShortenService.getOriginalUrlByShortenUrlKey(shortenUrlKey);
-        URI redirectUri = new URI(originalUrl);
-        HttpHeaders httpHeaders =new HttpHeaders();
-        httpHeaders.setLocation(redirectUri);
-
-        return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
-
-    }
-    @RequestMapping(value = "/shortenUrl",method = RequestMethod.POST)
-    public ResponseEntity<ShortenUrlCreateResponseDto>  createShortenUrl(@Valid @RequestBody ShortenUrlCreateRequestDto shortenUrlCreateRequestDto){
-        ShortenUrlCreateResponseDto shortenUrlCreateResponseDto=
-                simpleShortenService.generateShortUrl(shortenUrlCreateRequestDto);
-        return ResponseEntity.ok().body(shortenUrlCreateResponseDto);
-    }
-
-
-    @RequestMapping(value = "/shortenUrl/{shortenUrlKey}", method = RequestMethod.GET)
-    public ResponseEntity<?>  getShortenUrlInformation(        @PathVariable String shortenUrlKey){
-        ShortenUrlInformationDto shortenUrlInformationDto=
-            simpleShortenService.getShortenUrlInformationByShortenUrlKey(shortenUrlKey);
-        return ResponseEntity.ok(shortenUrlInformationDto);
-    }
-
-}
